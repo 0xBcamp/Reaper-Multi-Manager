62279b46feb77263f4637fc76293e3faf7a34384
Gerdus Marais <gerdusx@gmail.com>
2023-08-31 22:57:38 +0200

    Sidebar items, coming soon pages

--- multi-manager-dashboard/src/components/sidebar/SideBar.tsx
+++ multi-manager-dashboard/src/components/sidebar/SideBar.tsx
@@ -1,9 +1,11 @@
 import { Link } from 'react-router-dom'
+import { routes } from '../../utils/routes'
+import SideBarItem from './SideBarItem'
 
 function SideBar() {
   return (
     <div className='bg-white h-screen border-r-gray-200 border-r'>
-      <div className='p-3 flex justify-center text-xl font-semibold'>
+      <div className='p-3 flex justify-center text-xl font-semibold mb-4'>
         <Link to="/">
           <div className='flex flex-row space-x-2 items-center hover:text-gray-500 transition-colors duration-200'>
             <img src="icons/strategy-development.png" alt='project logo' className='h-[35px] hover:cursor-pointer' />
@@ -11,6 +13,13 @@ function SideBar() {
           </div>
         </Link>
       </div>
+      <div className='flex flex-col'>
+          {routes.map((route, index) => {
+            return (
+              <SideBarItem route={route} key={index}/>
+            )
+          })}
+        </div>
     </div>
   )
 }
