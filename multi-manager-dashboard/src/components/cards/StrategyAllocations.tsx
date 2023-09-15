b5658668e39120848d6774e94e923fc2fdd1627d
crypto-yuvi <yuvi.zeroalpha@gmail.com>
2023-09-15 13:42:24 +1000

    refactored strategyAllocations
    refactored strategyAllocations
    introduced calculateStrategyAllocations

--- multi-manager-dashboard/src/components/cards/StrategyAllocations.tsx
+++ multi-manager-dashboard/src/components/cards/StrategyAllocations.tsx
@@ -1,10 +1,20 @@
 // Allocation.tsx
-import { useMemo, useState } from 'react';
+import { useState } from 'react';
 import { formatUnits } from 'ethers';
-import { calculateDataWithThreshold, calculateTimeBasedMovingAverage } from '../../lib/calculateStrategyAPR';
 import { Vault } from '../../gql/graphql';
 import { StrategySelector } from '../../redux/selectors';
 import { DEFAULT_STD_DEV_THRESHOLD } from '../../utils/constants';
+import {
+    getLastStrategyHarvest,
+    getLastStrategyAllocated,
+    getStrategyAPRValues,
+    getStrategyAllocatedValues,
+    calculateStrategyProductValues,
+    calculateVaultAPR,
+    calculateActualAllocatedBPS,
+    calculateOptimumAllocation,
+    calculateOptimumAllocationBPS,
+} from '../../lib/calculateStrategyAllocations';
 
 interface IAllocationProps {
     vault: Vault;
@@ -15,26 +25,19 @@ interface IAllocationProps {
 const AllocationSummary = ({ vault, strategy, strategies }: IAllocationProps) => {
     const [threshold, setThreshold] = useState(DEFAULT_STD_DEV_THRESHOLD)
 
-    const lastVaultAllocated = vault.lastSnapShot?.totalAllocated;
-    
-    const lastStrategyHarvest = strategy.aprReports?.length > 0 ? strategy.aprReports[strategy.aprReports?.length - 1] : undefined;
-    const lastStrategyAllocated = lastStrategyHarvest?.allocated;
+    const lastStrategyHarvest = getLastStrategyHarvest(strategy);
+    const lastStrategyAllocated = getLastStrategyAllocated(strategy);
 
     const strategyAPR = strategy.APR;
 
-    const strategyAPRValues = strategies.map((strategy) => {
-        return strategy.APR;
-    });
+    const lastVaultAllocated = parseFloat(vault.lastSnapShot?.totalAllocated || "0");
+    const strategyAPRValues = getStrategyAPRValues(strategies);
+    const strategyAllocatedValues = getStrategyAllocatedValues(strategies);
+    const strategyProductValues = calculateStrategyProductValues(strategyAPRValues, strategyAllocatedValues);
+    const vaultAPR = calculateVaultAPR(strategyProductValues, lastVaultAllocated);
 
-    const strategyAllocatedValues: number[] = strategies.map((strat) => {
-        return parseFloat(strat.aprReports?.[strat.aprReports.length - 1]?.allocated || "0");
-    });
-
-    const strategyProductValues = strategies.map((_, index) => {
-        const allocatedValue = (strategyAllocatedValues[index]);
-        const aprValue = (strategyAPRValues[index]);
-        return allocatedValue * aprValue;
-    });
+    const optimumAllocation = calculateOptimumAllocation(parseFloat(lastStrategyAllocated), strategyAPR, vaultAPR);
+    const optimumAllocationBPS = calculateOptimumAllocationBPS(parseFloat(lastStrategyAllocated), strategyAPR, vaultAPR, lastVaultAllocated);
 
     const totalProductValue = strategyProductValues
         .reduce((total, productValue) => {
@@ -42,18 +45,14 @@ const AllocationSummary = ({ vault, strategy, strategies }: IAllocationProps) =>
             }
         );
 
-    const vaultAPR = totalProductValue / Number(lastVaultAllocated);
 
     return (
         <>
             {lastStrategyHarvest ?
-                <div className='flex flex-col p-2 text-gray-600 text-xs'>
-                    <div className='flex justify-between'>
-                    <div>CALCULATIONS</div>
-                </div>
+            <div className='flex flex-col p-2 text-gray-600 text-xs'>
                 <div className='flex justify-between'>
                     <div>Strategy allocated value:</div>
-                    <div>{parseFloat(formatUnits(lastStrategyAllocated))?.toFixed(2)}</div>
+                    <div>{parseFloat(formatUnits(lastStrategyAllocated)).toFixed(2)}</div>
                 </div>
                 <div className='flex justify-between'>
                     <div>Contract allocated BPS:</div>
@@ -61,23 +60,23 @@ const AllocationSummary = ({ vault, strategy, strategies }: IAllocationProps) =>
                 </div>
                 <div className='flex justify-between'>
                     <div>Actual allocated BPS:</div>
-                    <div>{(parseFloat(lastStrategyAllocated)/parseFloat(lastVaultAllocated)*10000).toFixed(2)}</div>
+                    <div>{(parseFloat(lastStrategyAllocated)/lastVaultAllocated*10000)?.toFixed(2)}</div>
                 </div>
                 <div className='flex justify-between'>
-                    <div>Apr: </div>
+                    <div>APR:</div>
                     <div>{(strategyAPR)?.toFixed(2)}%</div>
                 </div>
                 <div className='flex justify-between'>
-                    <div>Vault APR: </div>
-                    <div>{vaultAPR.toFixed(2)}</div>
+                    <div>Vault APR:</div>
+                    <div>{(vaultAPR)?.toFixed(2)}%</div>
                 </div>
                 <div className='flex justify-between'>
-                    <div>Optimum Allocation: </div>
-                    <div>{(parseFloat(formatUnits(lastStrategyAllocated))*strategyAPR/vaultAPR).toFixed(2)}</div>
+                    <div>Optimum Allocation:</div>
+                    <div>{parseFloat(formatUnits(optimumAllocation)).toFixed(2)}</div>
                 </div>
                 <div className='flex justify-between'>
-                    <div>Optimum Allocation BPS: </div>
-                    <div>{(parseFloat(lastStrategyAllocated)*strategyAPR/vaultAPR/parseFloat(lastVaultAllocated)*10000).toFixed(2)}</div>
+                    <div>Optimum Allocation BPS:</div>
+                    <div>{optimumAllocationBPS}</div>
                 </div>
             </div> :
             <div >No harvests found</div>}
