7baed12a0b305b45ffb7bfd4ac06a869a6db8d2d
Gerdus Marais <gerdusx@gmail.com>
2023-10-12 11:56:41 +0200

    Strategy apr summary update

--- multi-manager-dashboard/src/components/cards/Card.tsx
+++ multi-manager-dashboard/src/components/cards/Card.tsx
@@ -1,13 +1,13 @@
 
-type Props = {
-    title: string;
+interface ICardProps {
+    title?: string;
     children: JSX.Element;
 }
 
-function Card({ title, children }: Props) {
+function Card({ title, children }: ICardProps) {
     return (
         <div className='bg-white flex flex-col p-1 border-gray-200 border'>
-            <div className='p-2 text-gray-400 overflow-hidden text-ellipsis text-xs mb-1'>{title}</div>
+            {title && <div className='p-2 text-gray-400 overflow-hidden text-ellipsis text-xs mb-1'>{title}</div>}
             <div className='min-h-[100px]'>
                 {children}
             </div>
