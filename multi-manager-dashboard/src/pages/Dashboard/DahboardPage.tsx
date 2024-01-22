1df8fbb0a67686a051e6001103d4ee8496438216
Gerdus Marais <gerdusx@gmail.com>
2024-01-22 10:33:30 +0200

    dashboard - sort vaults

--- multi-manager-dashboard/src/pages/Dashboard/DahboardPage.tsx
+++ multi-manager-dashboard/src/pages/Dashboard/DahboardPage.tsx
@@ -7,6 +7,9 @@ import SnapshotContainer from '../../components/SnapshotCard/SnapshotContainer';
 import UnsupportedChain from '../../components/UnsupportedChain';
 import NoWalletConnected from '../../components/NoWalletConnected';
 import { Link } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+import Dropdown, { DropdownOptionType } from '../../components/form/Dropdown';
+import { Vault } from '../../redux/slices/vaultsSlice';
 
 const DahboardPage = () => {
     const isInitialized = useSelector((state: RootState) => state.app.isInitialized);
@@ -15,6 +18,41 @@ const DahboardPage = () => {
     const chain = useSelector(selectChain);
     const wallet = useSelector(selectWallet);
 
+    const [sortedVaults, setSortedVaults] = useState<Vault[]>([]);
+
+    const [vaultSortOptions, setVaultSortOptions] = useState([
+        { label: "TVL", key: "tvl" },
+        { label: "Health Score", key: "health" },
+        { label: "Name", key: "name" },
+        { label: "APR", key: "apr" },
+    ]);
+
+    const [vaultSortSelectedKey, setVaultSortSelectedKey] = useState<string>("tvl");
+
+    const handleVaultSortChange = (key: string) => {
+        setVaultSortSelectedKey(key);
+    };
+
+    // Effect to sort vaults whenever the vaults or selected sort key changes
+    useEffect(() => {
+        const sorted = [...vaults].sort((a, b) => {
+            switch (vaultSortSelectedKey) {
+                case 'name':
+                    return a.name.localeCompare(b.name);
+                case 'tvl':
+                    return b.lastSnapShot.usd.tvl - a.lastSnapShot.usd.tvl;
+                case 'health':
+                    return b.healthScore - a.healthScore;
+                case 'apr':
+                    return b.totalAPR - a.totalAPR;
+                default:
+                    return 0;
+            }
+        });
+        console.log("sorted", sorted)
+        setSortedVaults(sorted);
+    }, [vaults, vaultSortSelectedKey]);
+
     return (
         <div>
             {!isInitialized && <Loader />}
@@ -22,11 +60,11 @@ const DahboardPage = () => {
             {isInitialized && chain && wallet.status === "connected" &&
                 <div>
                     <div className="bg-white p-3 shadow-md">
-                            <div className="flex justify-between items-center">
-                                <div className="text-gray-600 font-bold">Dashboard</div>
-                                <div className="space-x-4">
-                                    <Link to={"/vaults/deploy"} className="text-blue-500 hover:text-blue-700" >Add Vault</Link>
-                                </div>
+                        <div className="flex justify-between items-center">
+                            <div className="text-gray-600 font-bold">Dashboard</div>
+                            <div className="space-x-4">
+                                <Link to={"/vaults/deploy"} className="text-blue-500 hover:text-blue-700" >Add Vault</Link>
+                            </div>
                         </div>
                     </div>
                     <div className='grid grid-cols-2 gap-4 p-4'>
@@ -48,11 +86,14 @@ const DahboardPage = () => {
                         />
                     </div>
 
-                    <div className='px-4 py-2 text-gray-800 text-lg'>
-                        Vaults
+                    <div className='flex justify-between px-4 py-2 text-gray-800 text-lg items-center'>
+                        <div>Vaults</div>
+                        <div>
+                            <Dropdown options={vaultSortOptions} onChange={handleVaultSortChange} selectedKey={vaultSortSelectedKey} />
+                        </div>
                     </div>
                     <div className='grid grid-cols-4 gap-4 px-4'>
-                        {vaults.map((vault) => {
+                        {sortedVaults.map((vault) => {
                             return (
                                 <DashboardVaultSummary vault={vault} key={vault._id} />
                             )
@@ -72,8 +113,8 @@ export const TVLTooltip = ({ active, payload, label }: any) => {
     if (active && payload && payload.length) {
 
         const date = new Date(label * 1000);
-		const formatter = new Intl.DateTimeFormat('en-US', { month: 'short', day: 'numeric' });
-		const formattedDate = formatter.format(date);
+        const formatter = new Intl.DateTimeFormat('en-US', { month: 'short', day: 'numeric' });
+        const formattedDate = formatter.format(date);
 
         return (
             <div className="p-2 bg-white shadow rounded border border-gray-300">
@@ -89,8 +130,8 @@ export const UsersTooltip = ({ active, payload, label }: any) => {
     if (active && payload && payload.length) {
 
         const date = new Date(label * 1000);
-		const formatter = new Intl.DateTimeFormat('en-US', { month: 'short', day: 'numeric' });
-		const formattedDate = formatter.format(date);
+        const formatter = new Intl.DateTimeFormat('en-US', { month: 'short', day: 'numeric' });
+        const formattedDate = formatter.format(date);
 
         return (
             <div className="p-2 bg-white shadow rounded border border-gray-300">
